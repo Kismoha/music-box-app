00c92baf9f726dc7775e30ccb5851530f087f8ac
Kismoha <kismoga@hotmail.com>
2019-05-10 12:54:57 +0200

    Implemented server main.

--- MusicBoxApp/src/server/main/MusicBox.java
+++ MusicBoxApp/src/server/main/MusicBox.java
@@ -5,14 +5,33 @@
  */
 package server.main;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import server.connectionHandlers.ClientConnection;
+import server.connectionHandlers.ConnectionsManager;
+
 /**
  *
  * @author kismo
  */
 public class MusicBox {
     
+    public static final int PORT = 40000;
+    
     public static void main(String[] args){
         
+        ConnectionsManager mgr = new ConnectionsManager();
+        
+        try(ServerSocket server = new ServerSocket(PORT);){
+            while(true){
+                ClientConnection conn = new ClientConnection(mgr,server.accept());
+                conn.startCommunication();
+            }
+        } catch (IOException ex) {
+            System.out.println("IO ex");
+        }
         
     }
 }
